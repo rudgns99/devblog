0c07f186230bbf0cdc8241e0b424e3e4c1780cd0
Mark Kim <46011169+rudgns99@users.noreply.github.com>
2021-03-12 15:54:31 +0900

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ module.exports = {
     linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://rudgns99.github.io/devblog/',
 };
